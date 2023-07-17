fcd612c9bc8757f59453dbee911c720f0ce8a396
Fayas Akram <fayas@cinergix.com>
2023-07-17 10:05:00 +0530

    minor changes added

--- src/app/components/GetStartedSection.jsx
+++ src/app/components/GetStartedSection.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const GetStartedSection = () => {
   return (
     <div className='md:h-[100vh] w-full md:px-32 px-8 mt-8 flex flex-col justify-center py-6 md:py-0'>
-    <h1 className='text-center mb-4'>Start scaling your<br/> business with Corzent</h1>
-    <p className='text-center mb-4'>Grow faster with a team that helps you to convert your ideas to product.</p>
+    <h1 className='text-center mb-4'>Start Expanding your<br/> business with ClanCode Labs</h1>
+    <p className='text-center mb-4'>Accelerate your growth with a team that turns your ideas into remarkable products.</p>
     <button className='self-center bg-[#45C1CE] px-16 py-8 rounded-full font-bold text-xl text-[#004450]'>Get Started Now</button>
     </div>
   )
--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -4,10 +4,9 @@ const HeroSection = () => {
   return (
     <div className='md:flex bg-[#DAF3F4] md:h-[90vh] w-full md:px-32 px-8 py-6 md:py-0'>
     <div className='md:h-screen flex flex-col justify-center'>
-        <h1>Build faster,</h1>
-        <h1>together</h1>
-        <h1>with Corzent</h1>
-        <p className='text-2xl my-4'>Product engineering simplified</p>
+        <h1>Expand,Grow and Elevate</h1>
+        <h1>your tech team with us.</h1>
+        <p className='text-2xl my-4'>We empowere numerous organizations to achieve business success through our expertise by engineering digital products.</p>
         <button className='text-2xl font-medium bg-[#45C1CE] py-6 px-12 rounded-full mt-4 md:self-start'><span>Get Started</span><span><FaArrowRight className='inline-flex items-center ml-3 justify-center'/></span></button>
     </div>
 
