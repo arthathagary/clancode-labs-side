c5b2971a4662f0683d03e5740eb7516b56541af1
Arthath Agary <arthathagary@gmail.com>
2023-07-20 15:56:27 +0530

    Footer Done

--- src/app/components/MvpSection.jsx
+++ src/app/components/MvpSection.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import Image from 'next/image'
+import webAppProjectImg from '../../../public/assets/images/webAppProject.jpg'
 
 const MvpSection = () => {
   return (
-    <div>MvpSection</div>
+    <div className='md:flex md:h-[70vh] w-full md:px-32 px-8 py-6 md:py-0 justify-center items-center gap-8'>
+        <div className='md:w-[50%]'>
+            <Image src={webAppProjectImg}  alt='webAppProjectImg'/>
+        </div>
+
+        <div className='md:w-[50%]'>
+            <h3>Minimum Value Products</h3>
+            <h2>We bulid lightweight, scalable MVPs</h2>
+            <p>We help you to Go-To-Market fast, continue to iterate and scale up while you grow</p>
+            <button>Explore More</button>
+        </div>
+    </div>
   )
 }
 
--- src/app/components/ProductDevelpment.jsx
+++ src/app/components/ProductDevelpment.jsx
@@ -4,12 +4,12 @@ import productDevelopmentImg from '../../../public/assets/images/productDevelopm
 
 const ProductDevelpment = () => {
   return (
-    <div className='md:flex bg-[#DAF3F4] md:h-[90vh] w-full md:px-32 px-8 py-6 md:py-0 justify-center items-center gap-8'>
+    <div className='md:flex bg-[#DAF3F4] md:h-[70vh] w-full md:px-32 px-8 py-6 md:py-0 justify-center items-center gap-8'>
     <div className='w-[50%]'>
     <h3 className='font-bold md:mb-4'>Product Development</h3>
     <h2>Innovative solutions Solid engineering</h2>
     <p className='pb-6'>We complement your vision of building the right product, by helping you build it the right way</p>
-    <button className='text-[#004450]'>Explore More</button>
+    <button className='text-[#45C0CE]'>Explore More</button>
     </div>
         
     <div className='md:w-[50%]'>
