50c8994867d2319ea8f5b8ea1288bbd88e1b768f
Arthath Agary <arthathagary@gmail.com>
2023-07-31 11:30:42 +0530

    qAndA section improved

--- src/app/components/QaSection.jsx
+++ src/app/components/QaSection.jsx
@@ -1,20 +1,22 @@
-import React from 'react'
+'use client';
+import React, { useState } from 'react'
 import { AiOutlinePlus} from "react-icons/ai";
+import generalQandA from '../assets/contents/generalQandA';
+import onBoardingQandA from '../assets/contents/onBoardingQandA';
 
 const QaSection = () => {
   return (
-    <div className='md:h-[80vh] w-full md:px-32 px-8 mt-8 md:flex items-center gap-16'>
+    <div className='md:h-[70vh] w-full md:px-32 px-8 mt-8 md:flex gap-16'>
     <div className='bg-[#C5ECF0] p-8 md:w-[50%]'>
         <h4 className='text-2xl font-semibold mb-4'>General</h4>
         
             <div>
-                <h5 className='flex items-center justify-between gap-4 text-xl font-medium mb-4'><span>Do you build MVP?</span><span><AiOutlinePlus/></span></h5>
+            {generalQandA.map((gQandA)=>{
+              return <div key={gQandA.id}> <h5 className='flex items-center justify-between gap-4 font-extrabold mb-4'><span>{gQandA.question}</span><span><AiOutlinePlus/></span></h5>
+                <p className='hidden'>{gQandA.answer}</p> 
                 <hr className="w-full h-[2px] mx-auto my-4 bg-gray-100 border-0 rounded md:my-10 dark:bg-gray-700"/>
-                <h5 className='flex items-center justify-between gap-4 text-xl font-medium mb-4'><span>I already have a team. Can you help expand?</span><span><AiOutlinePlus/></span></h5>
-                <hr className="w-full h-[2px] mx-auto my-4 bg-gray-100 border-0 rounded md:my-10 dark:bg-gray-700"/>
-                <h5 className='flex items-center justify-between gap-4 text-xl font-medium mb-4'><span>We need an app to go with our website. Can you handle that?</span><span><AiOutlinePlus/></span></h5>
-                <hr className="w-full h-[2px] mx-auto my-4 bg-gray-100 border-0 rounded md:my-10 dark:bg-gray-700"/>
-                <h5 className='flex items-center justify-between gap-4 text-xl font-medium mb-4'><span>I need a good product within a fortnight. Help!</span><span><AiOutlinePlus/></span></h5>
+                </div>
+            })}
             </div>
            
         
@@ -24,18 +26,18 @@ const QaSection = () => {
         <h4 className='text-2xl font-semibold mb-4'>General</h4>
         
             <div>
-                <h5 className='flex items-center justify-between gap-4 text-xl font-medium mb-4'><span>Do you build MVP?</span><span><AiOutlinePlus/></span></h5>
+            {onBoardingQandA.map((oQandA)=>{
+              return <div key={oQandA.id}> <h5 className='flex items-center justify-between gap-4 font-extrabold mb-4'><span>{oQandA.question}</span><span><AiOutlinePlus/></span></h5>
+                <p className='hidden'>{oQandA.answer}</p> 
                 <hr className="w-full h-[2px] mx-auto my-4 bg-gray-100 border-0 rounded md:my-10 dark:bg-gray-700"/>
-                <h5 className='flex items-center justify-between gap-4 text-xl font-medium mb-4'><span>I already have a team. Can you help expand?</span><span><AiOutlinePlus/></span></h5>
-                <hr className="w-full h-[2px] mx-auto my-4 bg-gray-100 border-0 rounded md:my-10 dark:bg-gray-700"/>
-                <h5 className='flex items-center justify-between gap-4 text-xl font-medium mb-4'><span>We need an app to go with our website. Can you handle that?</span><span><AiOutlinePlus/></span></h5>
-                <hr className="w-full h-[2px] mx-auto my-4 bg-gray-100 border-0 rounded md:my-10 dark:bg-gray-700"/>
-                <h5 className='flex items-center justify-between gap-4 text-xl font-medium mb-4'><span>I need a good product within a fortnight. Help!</span><span><AiOutlinePlus/></span></h5>
+                </div>
+            })}
             </div>
            
         
     </div>
     </div>
+   
   )
 }
 
