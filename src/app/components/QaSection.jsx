eb3979aad5817a24117ac5c1fd048b1899f23160
Arthath Agary <arthathagary@gmail.com>
2023-07-31 22:54:32 +0530

    Click to expand and close done on QandA

--- src/app/components/QaSection.jsx
+++ src/app/components/QaSection.jsx
@@ -5,15 +5,33 @@ import generalQandA from '../assets/contents/generalQandA';
 import onBoardingQandA from '../assets/contents/onBoardingQandA';
 
 const QaSection = () => {
+  const [click,setClick] = useState(null);
+  const [click2,setClick2] = useState(null);
+  const handleClick = (index)=> {
+    if(click === index){
+      setClick(null);
+    }else{
+      setClick(index);
+    }
+    
+  }
+  const handleClick2 = (i)=> {
+    if(click2 === i){
+      setClick2(null);
+    }else{
+      setClick2(i);
+    }
+    
+  }
   return (
     <div className='md:h-[70vh] w-full md:px-32 px-8 mt-8 md:flex gap-16'>
     <div className='bg-[#C5ECF0] p-8 md:w-[50%]'>
         <h4 className='text-2xl font-semibold mb-4'>General</h4>
         
             <div>
-            {generalQandA.map((gQandA)=>{
-              return <div key={gQandA.id}> <h5 className='flex items-center justify-between gap-4 font-extrabold mb-4'><span>{gQandA.question}</span><span><AiOutlinePlus/></span></h5>
-                <p className='hidden'>{gQandA.answer}</p> 
+            {generalQandA.map((gQandA,index)=>{
+              return <div key={gQandA.id}> <h5 className='flex items-center justify-between gap-4 font-extrabold mb-4' onClick={()=>handleClick(index)}><span>{gQandA.question}</span><span className='text-xl cursor-pointer'>{click===index?'-':'+'}</span></h5>
+                <h6 className={click === index ?'block':'hidden'}>{gQandA.answer}</h6> 
                 <hr className="w-full h-[2px] mx-auto my-4 bg-gray-100 border-0 rounded md:my-10 dark:bg-gray-700"/>
                 </div>
             })}
@@ -23,12 +41,12 @@ const QaSection = () => {
     </div>
 
     <div className='bg-[#C5ECF0] p-8 md:w-[50%]'>
-        <h4 className='text-2xl font-semibold mb-4'>General</h4>
+        <h4 className='text-2xl font-semibold mb-4'>Onboarding</h4>
         
             <div>
-            {onBoardingQandA.map((oQandA)=>{
-              return <div key={oQandA.id}> <h5 className='flex items-center justify-between gap-4 font-extrabold mb-4'><span>{oQandA.question}</span><span><AiOutlinePlus/></span></h5>
-                <p className='hidden'>{oQandA.answer}</p> 
+            {onBoardingQandA.map((oQandA,i)=>{
+              return <div key={oQandA.id}> <h5 className='flex items-center justify-between gap-4 font-extrabold mb-4' onClick={()=>handleClick2(i)}><span>{oQandA.question}</span><span className='text-xl cursor-pointer'>{click2===i?'-':'+'}</span></h5>
+                <h6 className={click2 === i ?'block':'hidden'}>{oQandA.answer}</h6> 
                 <hr className="w-full h-[2px] mx-auto my-4 bg-gray-100 border-0 rounded md:my-10 dark:bg-gray-700"/>
                 </div>
             })}
