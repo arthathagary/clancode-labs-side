2954727de1adf450c830c0a90b9e00df12e6b4bf
Arthath Agary <arthathagary@gmail.com>
2023-08-21 17:39:06 +0530

    apos

--- src/app/components/StoriesToProductSection.jsx
+++ src/app/components/StoriesToProductSection.jsx
@@ -12,7 +12,7 @@ const StoriesToProductSection = () => {
     <div className='md:w-[50%]'>
         <div className='bg-[#FFFFFF] inline-block px-4 py-2 rounded-full mb-8'>Stories to Product</div>
         <h2>We are all about converting user journeys to product.</h2>
-        <p className='text-[#004450]'>As our engineering team work with you on bringing your idea to life, we help you understand your customers even better. We gather continuous feedback in our development process and help align the product to better solve your customers' pain points. As a technical partner, we help in choosing the right technology to solve the problem and go to market as fast as possible.</p>
+        <p className='text-[#004450]'>As our engineering team work with you on bringing your idea to life we help you understand your customers even better. We gather continuous feedback in our development process and help align the product to better solve your customers pain points. As a technical partner, we help in choosing the right technology to solve the problem and go to market as fast as possible.</p>
         <hr className="w-48 h-1 mx-auto my-4 bg-gray-100 border-0 rounded md:my-10 dark:bg-gray-700"/>
 
        
