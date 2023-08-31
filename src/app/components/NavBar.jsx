8383fce0bc97618bf5db0f2b2a43884998c01cc1
Arsath8597 <arsath8597@gmail.com>
2023-08-31 22:47:25 +0530

    design Navbar

--- src/app/components/NavBar.jsx
+++ src/app/components/NavBar.jsx
@@ -1,54 +1,86 @@
 'use client';
-import React,{useState} from 'react'
+import React,{useState,useEffect} from 'react'
 import {FaBars,FaTimes,FaArrowRight} from 'react-icons/fa'
 import Image from 'next/image'
 import Logo from '../../../public/assets/images/logo.svg'
+import Link from 'next/link'
 import ServiceDropDown from './ServiceDropDown';
 
 const NavBar = () => {
   const [nav,setNav] = useState(false);
+  const [color,setColor]=useState('transparent')
+  const [text,setText] =useState('white')
 
   const handleHamClick = ()=>setNav(!nav)
+
+useEffect(()=>{
+  const change=()=>{
+    if(window.scrollY>=90){
+
+      setColor('rgb(30, 30, 30)')
+      setText('white')
+    }
+    else{
+      setColor('transparent')
+      setText('	#330066 ')
+    }
+  }
+  window.addEventListener('scroll',change);
+},[])
 
   const [menu,setMenu] = useState(true);
   const handleMouseOver = ()=>{
     setMenu(!menu);
   }
   return (
-    <nav className='flex justify-between w-full h-[80px] items-center md:px-32 px-8 '>
-        <div className='md:w-[220px] w-48'><Image src={Logo} objectFit='cover' alt='logo'/></div>
 
-        {/* {Desktop Menus} */}
-        
-            <ul className='hidden md:flex gap-4'>
-                <a href=''><li className='text-[1.1rem] text-gray-600 hover:text-gray-950'>Home</li></a>
-                <a href='#aboutpage'><li className='text-[1.1rem] text-gray-600 hover:text-gray-950'>About</li></a>
-                <a href='#services' onMouseOver={handleMouseOver}><li className='text-[1.1rem] text-gray-600 hover:text-gray-950'>Services</li></a>
-                <a href='/'><li className='text-[1.1rem] text-gray-600 hover:text-gray-950'>Portfolio</li></a>
-                <a href=''><li className='text-[1.1rem] text-gray-600 hover:text-gray-950'>Careers</li></a>
-                <a href=''><li className='text-[1.1rem] text-gray-600 hover:text-gray-950'>Blog</li></a>
-            </ul>
-        
-        <button className='group hidden md:flex bg-[#E6ECEE] py-3 px-6 rounded-3xl text-[1.1rem] text-[#004450]'><span>Contact us</span><span><FaArrowRight className='inline-flex items-center ml-3 justify-center group-hover:rotate-90 group-hover:duration-300'/></span></button>
+<div style={{backgroundColor:`${color}`}} className='fixed z-40   left-0 top-0 w-full ease-in-out duration-500 '>
+  <div className=' w-full m-auto bg-transparent flex justify-evenly items-center p-4 text-black'>
+<Link href='#'> 
+          <div style={{color:`${text}`}}  className='w-[200px] bg-black'><Image src={Logo}/></div>
+        </Link>
+  <ul style={{color:`${text}`}} className='hidden md:flex md:text-xs lg:text-lg  '>
+    <li className='p-4'>
+      <Link href="#">Home</Link>
+    </li>
+    <li className='p-4 '>
+      <Link href="#">About</Link>
+    </li>
+   <li className='p-4 '><Link href="#">Service</Link></li>
+   <li className='p-4 '><Link href="#">Portfolio</Link></li>
+   <li className='p-4 '><Link href="#">Careers</Link></li>
+   <li className='p-4 '><Link href="#">Blog</Link></li>
+   <li className='p-4 '><Link href="#">Contact Us</Link></li>
+   
+  </ul>
 
-        {/* {Hamburger Button} */}
-        <div onClick={handleHamClick} className='z-10 md:hidden cursor-pointer'>
-        {!nav?<FaBars/>:<FaTimes/>}
-        </div>
+  
+  {/* mobile Icons */}
 
-        {/* {Mobile Menus} */}
-        
-        <ul className={!nav ? 'hidden': 'absolute top-0 left-0 w-full h-screen bg-[white] flex flex-col justify-center items-center'}>
-                <li className='py-6 text-4xl'>Home</li>
-                <li className='py-6 text-4xl'>About</li>
-                <li className='py-6 text-4xl'>Services</li>
-                <li className='py-6 text-4xl'>Portfolio</li>
-                <li className='py-6 text-4xl'>Careers</li>
-                <li className='py-6 text-4xl'>Blog</li>
-                <button className='py-6 text-4xl'>Contact Us</button>
-            </ul>
-        
-    </nav>
+  <div className=' md:hidden z-50 block cursor-pointer'  onClick={handleHamClick}>
+  
+        {!nav?<FaBars size={20} style={{color:`${text}`}} className='text-white'/>:<FaTimes size={20} style={{color:`${text}`}}/>}
+       
+  </div>
+  {/* Mobile menu */}
+  <div style={{color:`${text}`}} className={nav ? ' md:hidden  absolute top-0 right-0 bottom-0 left-0 flex justify-center items-center w-full h-screen bg-black  text-center ease-in-out duration-500':'md:hidden  absolute top-0 right-0 bottom-0 left-[-100%] flex justify-center items-center w-full h-screen bg-slate-500  text-center ease-in-out duration-500 '}>
+  <ul>
+    <li className='p-4 text-2xl hover:text-gray-900  text-white '>
+      <Link href="#">Home</Link>
+    </li>
+    <li className='p-4 text-2xl hover:text-gray-900  text-white  '>
+      <Link href="#">About</Link>
+    </li>
+   <li className='p-4 text-2xl hover:text-gray-900   text-white '><Link href="#">Service</Link></li>
+   <li className='p-4 text-2xl hover:text-gray-900   text-white '><Link href="#">Portfolio</Link></li>
+   <li className='p-4 text-2xl hover:text-gray-900   text-white '><Link href="#">Careers</Link></li>
+   <li className='p-4 text-2xl hover:text-gray-900   text-white '><Link href="#">Blog</Link></li>
+   <li className='p-4 text-2xl hover:text-gray-900   text-white '><Link href="#">Contact Us</Link></li>
+   
+  </ul>
+  </div>
+  </div>
+</div>
 
  
   )
