ac49f8a455aab9da39ee97cef6c7d3c61e6fdb0c
Arsath8597 <arsath8597@gmail.com>
2023-09-21 21:58:26 +0530

    adjust logo in footer page

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -5,7 +5,7 @@ import Image from 'next/image'
 import whiteLogo from '../../public/assets/images/logoWhite.svg'
 const Footer = () => {
   return (
-    <footer className='w-full px-20 lg:px-32 xl:px-10 z-20 lg:justify-evenly  lg:flex items-center bg-[#004450] lg:py-6 ' >
+    <footer className='w-full md:px-10 lg:px-32 xl:px-32 z-20 lg:justify-between  lg:flex items-center bg-[#004450] lg:py-6 ' >
         <div className='text-center    z-10'>
             <div className='flex justify-center items-center'  ><Image src={whiteLogo} width={260} alt='logo'/></div>
             <div className='flex gap-4 mb-4 justify-center mt-2'>
@@ -17,7 +17,7 @@ const Footer = () => {
         </div>
 
         <nav className='z-10  lg:mb-5'>
-          <ul className='lg:flex z-10  grid grid-cols-4 gap-3  '>
+          <ul className='lg:flex z-10 sm:pl-20 pl-10 lg:pl-0  grid grid-cols-4 gap-3  '>
             <li className='text-[white] font-medium'>Home</li>
             <li className='text-[white] font-medium'>About</li>
             <li className='text-[white] font-medium'>Services</li>
